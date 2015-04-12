239e54aa95376dddbf130908e219c10cc357b8b8
Maciej Skierkowski <maciej@skierkowski.com>
2015-04-12 10:34:42 -0700

    Reorder require with CodeClimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,10 @@ require "codeclimate-test-reporter"
 require 'rspec'
 require 'factor/connector/test'
 require 'factor/connector/runtime'
-require 'factor-connector-timer'
 
 CodeClimate::TestReporter.start if ENV['CODECLIMATE_REPO_TOKEN']
 
-Dir.glob('./lib/factor-connector-*.rb').each { |f| require f }
+require 'factor-connector-timer'
 
 RSpec.configure do |c|
   c.include Factor::Connector::Test
